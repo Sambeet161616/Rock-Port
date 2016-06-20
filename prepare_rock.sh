e1f7c279f5ffdcd11f21820bc7c2270469dc9aed
Sambeet161616 <b313036@iiit-bh.ac.in>
2016-06-20 05:54:07 +0530

    Patching RTT

--- prepare_rock.sh
+++ prepare_rock.sh
@@ -1,16 +1,16 @@
 ROCK_DIR=${BUILD_PREFIX}/rock
-. ${BUILD_PREFIX}/scripts/env.sh
+. ${BUILD_PREFIX}/Rock-Port_ana/env.sh
 
-if ! [ -d ${ROCK_DIR} ]; then
-   echo; echo "Uncompressing ${BUILD_PREFIX}/pkgs/rock_base.tar.bz2"
-   if ! [ -e ${BUILD_PREFIX}/pkgs/rock_base.tar.bz2 ]; then
-      exit
-   fi
+#if ! [ -d ${ROCK_DIR} ]; then
+ #  echo; echo "Uncompressing ${BUILD_PREFIX}/pkgs/rock_base.tar.bz2"
+  # if ! [ -e ${BUILD_PREFIX}/pkgs/rock_base.tar.bz2 ]; then
+   #   exit
+   #fi
    cd ${BUILD_PREFIX}
-   tar xfj ${BUILD_PREFIX}/pkgs/rock_base.tar.bz2
-   mv ${BUILD_PREFIX}/rock_base ${BUILD_PREFIX}/rock
-   ${BUILD_PREFIX}/scripts/files/fixPath.sh
-fi
+   #tar xfj ${BUILD_PREFIX}/pkgs/rock_base.tar.bz2
+   #mv ${BUILD_PREFIX}/rock_base ${BUILD_PREFIX}/rock
+   ${BUILD_PREFIX}/Rock-Port_Ana/files/fixPath.sh
+#fi
 
 cd ${ROCK_DIR}
 
@@ -23,7 +23,7 @@ rm -rf ${ROCK_DIR}/tools/typelib/build
 
 echo "Applying patches"
 cd ${ROCK_DIR}
-for i in `find ${BUILD_PREFIX}/scripts/files/rock/ -name *.patch`; do
+for i in `find ${BUILD_PREFIX}/Rock-Port_ana/files/rock/ -name *.patch`; do
 	patch -p1 -i $i; 
 done;
 
