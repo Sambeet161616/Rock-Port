0a4ee12d86499a84cb3920c381fb087a816300b3
Sambeet161616 <b313036@iiit-bh.ac.in>
2016-08-28 12:57:45 +0530

    Added environment variables

--- Tests/Eigen/BUILDENV.sh
+++ Tests/Eigen/BUILDENV.sh
@@ -1,3 +1,5 @@
- export RTEMS_MAKEFILE_PATH=/home/sambeet/NewRockPort/x86/Install/rtems/4.11.0-rc3/i386-rtems4.11/pc386
- export RTEMS_CUSTOM=/home/sambeet/NewRockPort/x86/Install/rtems/4.11.0-rc3/make/custom
- export RTEMS_SHARE=/home/sambeet/NewRockPort/x86/Install/rtems/4.11.0-rc3/share/rtems4.11
+export RTEMS_MAKEFILE_PATH=/home/sambeet/NewRockPort/x86/Install/rtems/4.11.0-rc3/i386-rtems4.11/pc386
+#export PATH=$PATH:/home/sambeet/NewRockPort/x86/Install/rtems/4.11.0-rc3/bin
+
+ 
+
